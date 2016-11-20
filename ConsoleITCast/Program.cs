f401ab2e621f5b15b50115439942e8294f103a4d
赵浩楠 <charleszhn@hotmail.com>
2016-11-20 20:37:15 +0800

    new隐藏父类成员

--- ConsoleITCast/Program.cs
+++ ConsoleITCast/Program.cs
@@ -16,7 +16,20 @@ namespace ConsoleITCast
             com.read();
             var m3 = new mp3();
             m3.player();
-            
+            var sd = new sdf();
+            sd.dd();
         }
+    }
+
+    class person
+    {
+        public  void dd() { Console.WriteLine("p-----"); }
+    }
+    class sdf : person
+    {
+        
+        public  void dd() { 
+            base.dd();
+            Console.WriteLine("s------"); }
     }
 }
