638c24323f950cdba77c6d569fccd1f68ee2e98d
赵浩楠 <charleszhn@hotmail.com>
2016-11-20 20:38:43 +0800

    修改

--- ConsoleITCast/Program.cs
+++ ConsoleITCast/Program.cs
@@ -16,20 +16,9 @@ namespace ConsoleITCast
             com.read();
             var m3 = new mp3();
             m3.player();
-            var sd = new sdf();
-            sd.dd();
+           
         }
     }
 
-    class person
-    {
-        public  void dd() { Console.WriteLine("p-----"); }
-    }
-    class sdf : person
-    {
-        
-        public  void dd() { 
-            base.dd();
-            Console.WriteLine("s------"); }
-    }
+   
 }
