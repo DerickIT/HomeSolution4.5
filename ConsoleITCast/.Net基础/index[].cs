89f3e0e4a60d6fb50ca18f12dd53e75e423ad3c4
赵浩楠 <charleszhn@hotmail.com>
2016-11-22 21:28:22 +0800

    索引

---
+++ ConsoleITCast/.Net基础/index[].cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.Net基础
+{
+    public class index__
+    {
+        private string[] name = new string[] { "zhn", "2", "3", "4", "5" };
+        public string this[int index]
+        { get { return name[index]; } set { name[index] = value; } }
+    }
+}
