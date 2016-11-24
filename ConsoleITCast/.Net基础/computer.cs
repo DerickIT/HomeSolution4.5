59da69d735ae70359784bc4c122ee76285cccc48
赵浩楠 <charleszhn@hotmail.com>
2016-11-24 21:46:02 +0800

    exception

--- ConsoleITCast/.Net基础/computer.cs
+++ ConsoleITCast/.Net基础/computer.cs
@@ -18,7 +18,15 @@ namespace ConsoleITCast.Net基础
        }
        public void write()
        {
-           
+           try
+           {
+
+           }
+           catch (NullReferenceException e)
+           {
+               //只能处理空引用异常
+               throw e;
+           }
            Disk.write();
        }
       
