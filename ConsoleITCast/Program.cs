540af8ed605608de6ecb3120dd88b540fb87e8d0
赵浩楠 <charleszhn@hotmail.com>
2016-11-25 20:49:12 +0800

    可变参数params

--- ConsoleITCast/Program.cs
+++ ConsoleITCast/Program.cs
@@ -18,6 +18,15 @@ namespace ConsoleITCast
            m3.player();
            
         }
+        public int add(params int[] arr)
+        {
+            int num=0;
+            for (int i = 0; i < arr.Length; i++)
+            {//如果有其他参数，可变参数必须放到最后一个
+                num += arr[i];
+            }
+            return num;
+        }
     }
 
    
