cad3e168b0c88413008561e34e6c6c87fe15aa36
赵浩楠 <charleszhn@hotmail.com>
2016-11-25 22:36:01 +0800

    可变参数params

--- ConsoleITCast/.Net基础/computer.cs
+++ ConsoleITCast/.Net基础/computer.cs
@@ -20,7 +20,14 @@ namespace ConsoleITCast.Net基础
        {
            try
            {
+               string s1 = "as";
+               string s2 = "as";
+               string s3 = s1 + s2;
+               string.Intern(s2 + s3);//对字符串刘勇
+               //拘留池作用
 
+               //stack上分配的内存不一致，堆上的内存是一样的，因为留用了intern
+               string.IsInterned(s2 + s3);
            }
            catch (NullReferenceException e)
            {
