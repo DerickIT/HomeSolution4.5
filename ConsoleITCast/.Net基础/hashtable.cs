c41ced6e5c16be22b1da340191cc950abe060fc8
赵浩楠 <charleszhn@hotmail.com>
2016-11-27 20:37:01 +0800

    集合与arraylist

---
+++ ConsoleITCast/.Net基础/hashtable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.Net基础
+{
+    class hashtable
+    {
+        public void Hashtables()
+        {
+            Hashtable hb = new Hashtable();
+            hb.Add(1, 2);//以键值对形式添加
+        }
+    }
+    class arraylist
+    {
+        //ArrayList arrlist = new ArrayList();
+        ArrayList a = new ArrayList();
+        public void sd()
+        {
+            a.Add(1);
+        }
+    }
+}
