b47e4d97c6c6dace082991e7bc6a02c5ebac605d
赵浩楠 <charleszhn@hotmail.com>
2016-11-30 21:19:54 +0800

    大文件读取

---
+++ ConsoleITCast/.Net基础/Filestream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.Net基础
+{
+    class Filestream
+    {
+        /// <summary>
+        /// 读取文件展现出来
+        /// </summary>
+        public void test()
+        {
+            FileStream fs = new FileStream("", FileMode.Open);
+            byte[] bytes = new byte[fs.Length];
+            int r = fs.Read(bytes, 0, bytes.Length);
+            fs.Close();
+            fs.Dispose();
+            string fas = UTF8Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
