b21e831b22aeff4d7ab9f25229926dca5e9d12c8
赵浩楠 <charleszhn@hotmail.com>
2016-11-30 19:58:02 +0800

    11-30

---
+++ ConsoleITCast/.Net基础/Files.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.Net基础
+{
+    class Files
+    {
+        public void test()
+        {
+            //file类判断文件是否存在，directory判断文件夹是否存在
+            
+            File.Create("");
+        }
+    }
+}
