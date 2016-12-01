4e0cd110b5100a834d7e61d7f8df297ba471623d
赵浩楠 <charleszhn@hotmail.com>
2016-12-01 20:53:58 +0800

    文件流加密

--- ConsoleITCast/.Net基础/Filestream.cs
+++ ConsoleITCast/.Net基础/Filestream.cs
@@ -20,6 +20,19 @@ namespace ConsoleITCast.Net基础
             FileStream fs = new FileStream("", FileMode.Open);
             byte[] bytes = new byte[fs.Length];//缓冲区
             int r = fs.Read(bytes, 0, bytes.Length);
+            long length = fs.Length;
+            while (true)
+            {
+                int rs = fs.Read(bytes, 0, bytes.Length);
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    bytes[i] = (byte)(byte.MaxValue - bytes[i]);///加解密操作
+                }
+                if (rs<0)
+                {
+                    break;
+                }
+            }
             fs.Close();
             fs.Dispose();
             string fas = UTF8Encoding.UTF8.GetString(bytes);
