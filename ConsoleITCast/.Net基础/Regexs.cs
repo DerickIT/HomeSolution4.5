2049c6e9e80a8ae12be2c0df49bf780305aef059
赵浩楠 <charleszhn@hotmail.com>
2016-12-03 17:52:54 +0800

    正则表达式

--- ConsoleITCast/.Net基础/Regexs.cs
+++ ConsoleITCast/.Net基础/Regexs.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleITCast.Net基础
@@ -9,7 +10,7 @@ namespace ConsoleITCast.Net基础
     /// <summary>
     /// 正则表达式联系
     /// </summary>
-    class Regexs
+    public class Regexs
     {
         /*
          * .除\n外的任意字符
@@ -23,12 +24,18 @@ namespace ConsoleITCast.Net基础
          * [^az$]必须要完全匹配
          * \d表示0-9，\D表示除了0-9的所有字符
          * \s表示看不到的空白符\S小写s的反面，除了看不到的空白符
+         * \w汉字，下划线，单词
          
          */
 
         public bool test()
         {
             var vl = "b[jtsu]g";//只能匹配到【】中的内容，就像bug，但不能匹配出brg
+            Regex rg = new Regex("", RegexOptions.IgnoreCase);
+            //Regex.IsMatch();判断字符串是否匹配相应的规则
+            //Regex.Match()从某个字符串中取出符合规则的字符
+            //Regex.Matches()字符串提取，可以提取所有的符合规则的字符串
+            //Regex.Replace()字符串替换，把所有匹配正则表达四的字符串替换成相应的字符串
             return false;
         }
     }
