62c68bcf63d5cb27b486414aa46b4a55e1571a9b
赵浩楠 <charleszhn@hotmail.com>
2016-12-04 15:08:54 +0800

    delegate

---
+++ ConsoleITCast/.Net基础/delegates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.Net基础
+{
+   public class delegates
+    {
+       public delegate void str(string val);
+       public void strs(string a)
+       {
+           
+       }
+       public void testdelegate(str d)
+       {
+           d.Invoke("s");
+       }
+    }
+}
