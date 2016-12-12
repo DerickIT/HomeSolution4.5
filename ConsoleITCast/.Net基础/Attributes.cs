e6ca63eb87ed6842d79683c4941fc9fbba3bdca0
赵浩楠 <charleszhn@hotmail.com>
2016-12-12 20:15:29 +0800

    attr

---
+++ ConsoleITCast/.Net基础/Attributes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.Net基础
+{
+
+    public class Attributes
+    {
+        [Objeces("人员id")]
+        public int ID { get; set; }
+    }
+    public class ObjecesAttribute : Attribute
+    {
+        public ObjecesAttribute(string a)
+        {
+            this.len = a;
+        }
+        public string len { get; set; }
+    }
+    public class funccs
+    {
+
+        public void func()
+        {
+            object o = new Attributes();
+            Type _type = o.GetType();
+            PropertyInfo[] pi = _type.GetProperties();
+            foreach (var item in pi)
+            {
+                object[] obj = item.GetCustomAttributes(typeof(Attributes), false);
+                if (obj.Length > 0)
+                {
+                    Attributes sdf = (Attributes)obj[0];
+                }
+            }
+
+        }
+    }
+}
