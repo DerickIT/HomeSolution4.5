94ee3cf968aa7d75ec5487cc5f6463e1faf5bcb0
赵浩楠 <charleszhn@hotmail.com>
2016-12-15 20:00:42 +0800

    kksk

---
+++ ConsoleITCast/SQL/UNcocheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class UNcocheck
+    {
+        /*
+         * 约束
+         * 唯一约束
+         * 主键约束
+         * 非空约束
+         * 
+         */
+    }
+}
