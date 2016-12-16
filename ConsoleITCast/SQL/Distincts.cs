f8345e177e99f068378f9d58f2d39fedf4415ff6
赵浩楠 <charleszhn@hotmail.com>
2016-12-16 21:19:21 +0800

    sql函数

---
+++ ConsoleITCast/SQL/Distincts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class Distincts
+    {
+        /*
+         * distinct是针对你查出的结果去重复
+         * max
+         * min
+         * count
+         * sum
+         * avg 计算平均分的时候对空值不处理 null
+         * between end
+         */
+    }
+}
