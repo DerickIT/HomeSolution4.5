270f1224781be90c7eb7190f1bff52739fa6ea5f
赵浩楠 <charleszhn@hotmail.com>
2016-12-17 20:13:23 +0800

    sql转换

---
+++ ConsoleITCast/SQL/SQL类型转换.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class SQL类型转换
+    {
+        /*
+         * 类型转换中通常用cast 或者convert
+         * cast:select cast(100 as varchar(30))+'hello'
+         * convert: select convert(datetime,getdate(),111)
+         */
+    }
+}
