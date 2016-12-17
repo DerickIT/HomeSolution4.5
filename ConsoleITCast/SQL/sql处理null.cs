a2ae74ea34eb9446ebc2c7e6aab3c1cb76f6d813
赵浩楠 <charleszhn@hotmail.com>
2016-12-17 18:54:05 +0800

    sql的执行顺序呢

--- ConsoleITCast/SQL/sql处理null.cs
+++ ConsoleITCast/SQL/sql处理null.cs
@@ -28,6 +28,25 @@ namespace ConsoleITCast.SQL
          * 排序之后是数据源 
          * select * from ta order by name desc,id asc
          *  默认是升序 asc
+         *  orderby 要放在where 字句之后
+         *  order by之后可以跟一个计算表达式
+         *  先执行group by 分组然后在统计每一组的信息
+         *  -------------------------------
+         * 一般分组语句group by都与 聚合函数配合使用
+         * 如果没有聚合函数，分组的意义不大
+         * 当在select语句中出现聚合函数时，这时不能再select中出现其他列，除非该列也在group by字句中出现或者在聚合函数中出现
+         */
+    }
+    class havings
+    {
+        /*
+         * having 在分组之后筛选 where是在分组之前筛选
+         * 分组之后筛选的是组
+         * select id as 数据id ,count(*) 总人数 from tbale where sex=0 group by id having count(*)>2 按照班级分组，并且筛选出人数》2
+         * having能使用哪列是取决于你分组后结果集有哪列
+         * ==============================
+         * SQL语句的执行顺序
+         * select from  where groupby having order by
          */
     }
 }
