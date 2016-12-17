18fbe50e4f89dd6c9f75a2db1dbb8309f007d62a
赵浩楠 <charleszhn@hotmail.com>
2016-12-17 20:37:58 +0800

    sql字符串操作

--- ConsoleITCast/SQL/SQL类型转换.cs
+++ ConsoleITCast/SQL/SQL类型转换.cs
@@ -28,4 +28,19 @@ namespace ConsoleITCast.SQL
          * insert into backstu select * from table  //backstu必须提前存在
          */
     }
+    class stringfunction
+    {
+        /*
+         * 字符串函数
+         * len(）计算字符串长度
+         * datalengtg 返回字节长度
+         * lower upper 转换大小写
+         * ltrim 去掉左边的空格 rtrim 去掉右面空格
+         * left(str,10),从左截取10个长度字符串
+         * right(str,10)从右
+         * substring（str,2,5）从左边开始截取第2-5个 索引从1开始数
+         * 
+         * 
+         */
+    }
 }
