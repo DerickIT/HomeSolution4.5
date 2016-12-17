b7788b899bc01687f50a7dd5e8fab93865a06f62
赵浩楠 <charleszhn@hotmail.com>
2016-12-17 20:33:49 +0800

    sql字符串操作

--- ConsoleITCast/SQL/SQL类型转换.cs
+++ ConsoleITCast/SQL/SQL类型转换.cs
@@ -14,4 +14,18 @@ namespace ConsoleITCast.SQL
          * convert: select convert(datetime,getdate(),111)
          */
     }
+    class unions
+    {
+        /*
+         * union将多个结果集联合
+         * union  all 不会去除重复数据
+         * union 会去除重复数据
+         * 当联合的时候注意列的类型
+         * 列的个数必须一致
+         * =================================
+         * select top 0 * into newtable from stutable 复制表结构重新创建一个newtable 表
+         * 如果表已经存在
+         * insert into backstu select * from table  //backstu必须提前存在
+         */
+    }
 }
