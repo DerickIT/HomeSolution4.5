08e355dc06df4381bb4bf0181a37c64f8610106a
赵浩楠 <charleszhn@hotmail.com>
2016-12-19 20:26:26 +0800

    sql子查询

---
+++ ConsoleITCast/SQL/sql子查询.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class sql子查询
+    {
+        /*
+         * 子查询分为独立子查询（可以自己独立完成的查询），相关子查询（子查询引用了父查询的结果）
+         * in exists not in 。。。
+         * 把一个查询结果作为另一个查询的查询源
+         * 
+         */
+    }
+}
