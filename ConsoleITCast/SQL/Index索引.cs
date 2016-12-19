16f60c5839854f8dd0659b0bf3d91ba725ff136e
赵浩楠 <charleszhn@hotmail.com>
2016-12-19 19:49:02 +0800

    索引index

---
+++ ConsoleITCast/SQL/Index索引.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class Index索引
+    {
+        /*
+         * 索引就是相当于生活中的字典，按照目录查
+         */
+    }
+}
