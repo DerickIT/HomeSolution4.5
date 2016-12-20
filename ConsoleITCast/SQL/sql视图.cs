7b553e58d6806110a5362176d12d47773a0a26a3
赵浩楠 <charleszhn@hotmail.com>
2016-12-20 19:45:22 +0800

    视图

---
+++ ConsoleITCast/SQL/sql视图.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class sql视图
+    {
+        /*
+         * 视图本身不存储数据，只是存储了查询语句
+         * 视图是一张虚拟表，表示一张表的部分数据或者多张表的综合数据
+         * 安全性：可以访问视图权限，但不给表的访问权限
+         */
+    }
+}
