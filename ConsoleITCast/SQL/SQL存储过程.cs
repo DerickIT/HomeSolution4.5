6b7a4dd7f7c2a2d0285c563a6c479cd384103b29
赵浩楠 <charleszhn@hotmail.com>
2016-12-22 19:53:12 +0800

    存储过程

---
+++ ConsoleITCast/SQL/SQL存储过程.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class SQL存储过程
+    {
+        /*
+         * 存储过程可以防止sql注入
+         * 输出参数用output标注就可输出
+         * 建议在创建proc的过程中加上begin  end
+         * create proc uu_proc
+         * @id int = 0;
+         * as
+         * begin
+         * ...执行sql语句
+         * end
+         */
+    }
+}
