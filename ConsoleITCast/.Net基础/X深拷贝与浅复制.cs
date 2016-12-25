68ca9f6b78baf53834117d27d06391595236e465
赵浩楠 <charleszhn@hotmail.com>
2016-12-25 14:58:23 +0800

    sql tran

--- ConsoleITCast/.Net基础/X深拷贝与浅复制.cs
+++ ConsoleITCast/.Net基础/X深拷贝与浅复制.cs
@@ -12,27 +12,27 @@ namespace ConsoleITCast.Net基础
          * 两种都对对象进行了复制，深拷贝相当于是重新创建了一个对象，浅复制只是复制了地址，重新开辟了空间
          */
 
-        public void sdf()
-        {
-            //浅复制
-            ptest qian = new ptest();
-            qian.Age = 13;
-            qian.name = "zhn";
-            ptest qiant = new ptest();
-            qiant.Age = qian.Age;
-        }
-        public void shen()
-        {
-            ptest shen = new ptest();
-            shen.Age = 13;
-            shen.name = "zhn";
-            ptest shens = new ptest();
-            shens.Age = shen.Age;
-            shens.name = shen.name;
+        //public void sdf()
+        //{
+        //    浅复制
+        //    ptest qian = new ptest();
+        //    qian.Age = 13;
+        //    qian.name = "zhn";
+        //    ptest qiant = new ptest();
+        //    qiant.Age = qian.Age;
+        //}
+        //public void shen()
+        //{
+        //    ptest shen = new ptest();
+        //    shen.Age = 13;
+        //    shen.name = "zhn";
+        //    ptest shens = new ptest();
+        //    shens.Age = shen.Age;
+        //    shens.name = shen.name;
 
-            //深拷贝
-            ptest shenss = shens;
-        }
+        //    //深拷贝
+        //    ptest shenss = shens;
+        //}
 
 
     }
---
+++ ConsoleITCast/SQL/tran事务.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.SQL
+{
+    class tran事务
+    {
+        public void method()
+        {
+            SqlTransaction sd = new SqlConnection().BeginTransaction();
+            SqlCommand COMM = new SqlCommand();
+            COMM.Transaction = sd;
+            sd.Rollback();
+            sd.Commit();
+            /*
+             * 声明事务，用conn.begintrancation
+             * cmd.trancation= 新声明的这个事务对象
+             * 可以使用sd.Rollback()()
+             * ========================
+             * sqlparameter 参数0需要把它转成object类型， 否则默认会把他识别为bigint类型，不能插入成功
+             */
+        }
+    }
+}
