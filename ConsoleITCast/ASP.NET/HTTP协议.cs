f99ef254ad18d213895f434a607726c5614e5b2d
赵浩楠 <charleszhn@hotmail.com>
2016-12-26 21:20:30 +0800

    http协议

---
+++ ConsoleITCast/ASP.NET/HTTP协议.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.ASP.NET
+{
+    class HTTP协议
+    {
+#if false
+        http协议
+        操作系统信息
+        编码信息
+        数据信息
+        cookie
+        响应头
+#endif
+    }
+}
