7bfa6e1afb245477e5f510e8b2f8ab3150b5882b
赵浩楠 <charleszhn@hotmail.com>
2016-12-27 20:16:32 +0800

    socket

---
+++ ConsoleITCast/ASP.NET/Socket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITCast.ASP.NET
+{
+    class Socket
+    {
+        /*
+         * 创建ip
+         * 创建socket
+         * 绑定一个端口，绑定本地ip
+         * 开始监听
+         * Accept（）
+         * 开启新的线程监听不同用户的访问端口
+         * 
+         */
+    }
+}
