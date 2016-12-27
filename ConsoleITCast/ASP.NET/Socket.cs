40562d70ab5798ff298c11f92c761215f5495aa4
赵浩楠 <charleszhn@hotmail.com>
2016-12-27 21:22:38 +0800

    socket

--- ConsoleITCast/ASP.NET/Socket.cs
+++ ConsoleITCast/ASP.NET/Socket.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Sockets;
 namespace ConsoleITCast.ASP.NET
 {
     class Socket
@@ -15,7 +15,18 @@ namespace ConsoleITCast.ASP.NET
          * 开始监听
          * Accept（）
          * 开启新的线程监听不同用户的访问端口
+         * Receive（）
+         * Listen（）
+         * send（）
          * 
+         *关闭： 服务器端出现异常表明客户端已经关闭，然后把socket关闭
+         * 
+         * 发送：socket  send()
          */
+
+        public void sdf()
+        { 
+        
+        }
     }
 }
